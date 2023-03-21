46ca4970d11af3b06b585b8a6417bc283b452d45
TheCrudman <betterwebco@gmail.com>
2023-03-21 16:19:36 +1300

    Add ability to save counts

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { useState } from 'react'
 import Button from './components/Button'
 
+
+
 function App() {
+  
+
   const [count, setCount] = useState(0)
+  const [history, setHistory] = useState([])
+
+  // const saveTotal = () => {
+  //   setHistory = [...history, count]
+  //   reset()
+  // }
 
   const increment = (step) => {
     console.log('here')
@@ -19,24 +29,45 @@ function App() {
     setCount(0)
   }
   return (
-    <div className="grid place-items-center h-screen bg-slate-200">
-      <div className="bg-white px-16 py-8 overflow-hidden rounded-lg shadow">
-        <h1 className="text-3xl">Counter: {count}</h1>
+    <div className="flex items-center justify-center h-screen bg-slate-200">
+      <div>
+        <div className="bg-white mx-8 px-10 py-8 overflow-hidden rounded-lg shadow">
+          <h1 className="text-3xl">Counter: {count}</h1>
+          <button
+            onClick={() => {
+              history.unshift(count)
+              reset()
+            }}
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-1 mt-2 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Save total
+          </button>
 
-        <h2 className="text-xl mb-2 mt-4">Add</h2>
-        <Button step={1} increment={increment} />
-        <Button step={10} increment={increment} />
-        <Button step={100} increment={increment} />
-        <h2 className="text-xl mb-2 mt-4">Subtract</h2>
-        <Button step={1} decrement={decrement} />
-        <Button step={10} decrement={decrement} />
-        <Button step={100} decrement={decrement} />
-        <p className="text-center mt-4">
-          {' '}
-          <a onClick={reset} className="cursor-pointer">
-            Reset
-          </a>
-        </p>
+          <h2 className="text-xl mb-2 mt-4">Add</h2>
+          <Button step={1} increment={increment} />
+          <Button step={10} increment={increment} />
+          <Button step={100} increment={increment} />
+          <h2 className="text-xl mb-2 mt-4">Subtract</h2>
+          <Button step={1} decrement={decrement} />
+          <Button step={10} decrement={decrement} />
+          <Button step={100} decrement={decrement} />
+          <p className="text-center mt-4">
+            {' '}
+            <a onClick={reset} className="cursor-pointer">
+              Reset
+            </a>
+          </p>
+        </div>
+        <div className="mx-8 px-10 py-8 w">
+          <h2 className="text-xl">Count History (latest first)</h2>
+          <ol className="list-decimal list-inside" reversed>
+            {history.map((value) => (
+              <li className="" key={value}>
+                {value}
+              </li>
+            ))}
+          </ol>
+        </div>
       </div>
     </div>
   )
